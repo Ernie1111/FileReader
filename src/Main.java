5740725da1d539d750bd5a5196bb26a5829361b8
Ernesto Huerta <ernestoh1111@hotmail.com>
2023-09-27 21:06:37 -0700

    This is my first Commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,35 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
 
 //Opens up file
         File filePath = new File("C:\\Users\\Dev\\Developer");
 //Reading what is in the file
         File[] listFiles = filePath.listFiles();
-//Prints the names of files
-        System.out.println("File Names: ");
-        for (int i = 0; i < listFiles.length; i++){
-            if(listFiles[i].isFile()){
-                System.out.println(listFiles[i].getName());
+//Prints the names of files in a text file
+
+        try {
+            FileWriter writer = new FileWriter("ListofFiles.txt");
+            for (int i = 0; i < listFiles.length; i++) {
+                if (listFiles[i].isFile()) {
+                    writer.write(listFiles[i].getName());
+
+                }
+
             }
         }
 
-
-
+        catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
-}
+
